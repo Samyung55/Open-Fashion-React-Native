925284872129590180a58a420c0a1f8c8523d7c7
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-10-12 08:48:13 +0100

    Create home.js

---
+++ app/home.js
@@ -0,0 +1,10 @@
+import { Text, Image, View } from "react-native";
+import Navbar from "../components/Navbar/Navbar"
+
+const Home = () => {
+    return (
+        <Navbar />
+    )
+}
+
+export default Home
