37256f744502ba42217f0117971b0164d7f666ed
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-10-12 12:04:27 +0100

    Update home.js

--- app/home.js
+++ app/home.js
@@ -1,9 +1,13 @@
 import { Text, Image, View } from "react-native";
 import Navbar from "../components/Navbar/Navbar"
+import Hero from "../components/Hero/Hero";
 
 const Home = () => {
     return (
+        <View style={{ flex: 1 }}>
         <Navbar />
+        <Hero />
+        </View>
     )
 }
 
