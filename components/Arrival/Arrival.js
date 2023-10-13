ede68e527052b0e3c5177eb8a99a7f1a75fb4091
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-10-13 16:12:21 +0100

    Update Arrival.js

--- components/Arrival/Arrival.js
+++ components/Arrival/Arrival.js
@@ -1,10 +1,23 @@
-import react, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 import * as Font from 'expo-font';
 
 const Arrival = () => {
 
     const [fontLoaded, setFontLoaded] = useState(false);
+    const [selectedItem, setSelectedItem] = useState(0);
+
+    const menuItems = [
+      { id: 1, label: 'All' },
+      { id: 2, label: 'Apparel' },
+      { id: 3, label: 'Dress' },
+      { id: 4, label: 'T-shirt' },
+      { id: 5, label: 'Bag' },
+    ];
+
+    const handleMenuItemClick = (item) => {
+      setSelectedItem(item);
+    };
 
     useEffect(() => {
       async function loadCustomFonts() {
@@ -18,18 +31,33 @@ const Arrival = () => {
       loadCustomFonts();
     }, []);
   
-    
     if (!fontLoaded) {
       return null; // Wait for the font to load before rendering
     }
 
     return (
+      <>
         <View style={{ justifyContent: "center", alignItems: "center" }}> 
             <Text style={styles.arrival}>
                 NEW ARRIVAL
             </Text>
             <Image style={styles.borderImg} source={require("../../assets/3.png")} />
+            <View style={{ flex: 1, flexDirection: "row", justifyContent: "space-between" }}>
+              {menuItems.map((item) => (
+                <TouchableOpacity
+                  key={item.id}
+                  onPress={() => handleMenuItemClick(item.id)}
+                  style={[
+                    styles.arrivalList,
+                    selectedItem === item.id && styles.selectedArrivalList,
+                  ]}
+                >
+                  <Text>{item.label}</Text>
+                </TouchableOpacity>
+              ))}
+            </View>
         </View>
+      </>
     )
 }
 
@@ -42,9 +70,21 @@ const styles = StyleSheet.create({
         marginTop: 35,
     },
     borderImg: {
-        marginBottom: 35,
+        marginBottom: 30,
         marginTop: -20
-    }
+    },
+    arrivalList: {
+      fontFamily: "TenorSans",
+      fontSize: 14,
+      marginRight: 16,
+      marginBottom: 16,
+      color: "#9E9E9E",
+    },
+    selectedArrivalList: {
+      color: "black",
+      borderBottomWidth: 2,
+      borderBottomColor: '#DD8560', // Adjust color as needed
+    },
 })
 
-export default Arrival
+export default Arrival;
