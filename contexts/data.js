a9412dc5f2a3b18ea4aec38cad87889fe96bc2c4
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-10-13 16:40:46 +0100

    Update data.js

--- contexts/data.js
+++ contexts/data.js
@@ -1,3 +1,31 @@
+import angora1 from "../assets/products/angora1"
+import angora2 from "../assets/products/angora2"
+import angora3 from "../assets/products/angora3"
+import angora4 from "../assets/products/angora4"
+
 export const ArrivalData = [
-    
+    {
+        id: 1,
+        name: "21WN reversible angora cardigan",
+        price: "$120",
+        productImage: angora1
+    },
+    {
+        id: 2,
+        name: "21WN reversible angora cardigan",
+        price: "$120",
+        productImage: angora2
+    },
+    {
+        id: 3,
+        name: "21WN reversible angora cardigan",
+        price: "$120",
+        productImage: angora3
+    },
+    {
+        id: 4,
+        name: "Oblong bag",
+        price: "$120",
+        productImage: angora4
+    },
 ]
