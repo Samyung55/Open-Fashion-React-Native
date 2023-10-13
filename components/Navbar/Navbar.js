881669e358698244e7e0defec6c5926a950dda47
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-10-13 15:07:11 +0100

    Update Navbar.js

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,11 +1,21 @@
 import { View, Text, StyleSheet, Image, Button } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const Navbar = () => {
    return (
     <View style={styles.container}>
-  <Image style={styles.image} source={require("../../assets/menu.png")} />
-  <Text style={styles.text}>This is Navbar</Text>
-  
+        <TouchableOpacity>
+            <Image style={{ width: 35, height: 27, marginLeft: -10 }} source={require("../../assets/menu.png")} />
+        </TouchableOpacity>
+        
+            <Image style={{ width: 100, height: 65, marginLeft: 70, marginRight: 20  }} source={require("../../assets/logo.png")} />
+        
+        <TouchableOpacity>
+        <Image style={{ width: 25, height: 25, marginLeft: 40  }} source={require("../../assets/search.png")} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+        <Image style={{ width: 22, height: 25, marginLeft: 25 }} source={require("../../assets/shopping-bag.png")} />
+        </TouchableOpacity>
 </View>
     )
 }
@@ -15,15 +25,19 @@ const styles = StyleSheet.create({
       backgroundColor: '#E7EAEF',
       flex: 0.08,
       justifyContent: "center",
-      flexDirection: "row"
+      flexDirection: "row",
+      alignItems: "center",
+      padding: 15
     },
     text: {
       fontSize: 16,
       color: 'brown',
     },
-    image: {
-        width: 50,
-        height: 50
+    image1: {
+        width: 35,
+        height:35,
+        marginRight: 0,
+        marginLeft: -120
     }
   });
   
