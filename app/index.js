a6fbd676867c267064ff92c1fe005dffca260784
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-10-14 08:41:28 +0100

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,5 +1,12 @@
 import { Redirect } from "expo-router";
 
+import { ProductContextProvider } from "../contexts/productContext"
+
+
 export default function Index() {
-    return <Redirect href="/home" />;
+    return (
+        <ProductContextProvider>
+    <Redirect href="/home" />
+    </ProductContextProvider>
+    )
 }
