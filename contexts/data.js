65cceb1ecfc2d3a8dacea0af3b7cf8008d9e139e
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-10-14 08:42:18 +0100

    Update data.js

--- contexts/data.js
+++ contexts/data.js
@@ -1,31 +1,98 @@
-import angora1 from "../assets/products/angora1"
-import angora2 from "../assets/products/angora2"
-import angora3 from "../assets/products/angora3"
-import angora4 from "../assets/products/angora4"
+const angora1 = require("../assets/products/angora1.png")
+const angora2 = require("../assets/products/angora2.png")
+const angora3 = require("../assets/products/angora3.png")
+const angora4 = require("../assets/products/angora4.png")
 
-export const ArrivalData = [
+const slide1 = require("../assets/products/harris.png")
+const slide2 = require("../assets/products/cashmere.png")
+const slide3 = require("../assets/products/harris2.png")
+
+export const arrivalData = [
     {
         id: 1,
         name: "21WN reversible angora cardigan",
         price: "$120",
-        productImage: angora1
+        productImage: angora1,
+        category: "Apparel"
     },
     {
         id: 2,
         name: "21WN reversible angora cardigan",
         price: "$120",
-        productImage: angora2
+        productImage: angora2,
+        category: "T-shirt"
     },
     {
         id: 3,
         name: "21WN reversible angora cardigan",
         price: "$120",
-        productImage: angora3
+        productImage: angora3,
+        category: "Dress"
     },
     {
         id: 4,
-        name: "Oblong bag",
+        name: "Harris Tweed Three button Jacket",
         price: "$120",
-        productImage: angora4
+        productImage: angora4,
+        category: "Bag"
+    },
+    {
+        id: 5,
+        name: "Harris Tweed Three button Jacket",
+        price: "$120",
+        productImage: slide1,
+        category: "Apparel"
+    },
+    {
+        id: 6,
+        name: "Cashmere Blend Cropped Jacket SW1WJ285-AM",
+        price: "$120",
+        productImage: slide2,
+        category: "T-shirt"
+    },
+    {
+        id: 7,
+        name: "Harris Tweed Three-button Jacket",
+        price: "$120",
+        productImage: slide3,
+        category: "Dress"
+    },
+    {
+        id: 8,
+        name: "21WN reversible angora cardigan",
+        price: "$120",
+        productImage: slide1,
+        category: "Bag"
+    },
+]
+
+export const foryouData = [
+    {
+        id: 1,
+        name: "Harris Tweed Three button Jacket",
+        price: "$120",
+        productImage: slide1,
+        category: "Apparel"
+    },
+    {
+        id: 2,
+        name: "Cashmere Blend Cropped Jacket SW1WJ285-AM",
+        price: "$120",
+        productImage: slide2,
+        category: "T-shirt"
+    },
+    {
+        id: 3,
+        name: "Harris Tweed Three-button Jacket",
+        price: "$120",
+        productImage: slide3,
+        category: "Dress"
+    },
+    {
+        id: 4,
+        name: "21WN reversible angora cardigan",
+        price: "$120",
+        productImage: slide1,
+        category: "Bag"
     },
 ]
