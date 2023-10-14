6bc7aa8bea097719e4b81d74ffcae70c0446e9f5
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-10-14 08:41:26 +0100

    Update home.js

--- app/home.js
+++ app/home.js
@@ -2,13 +2,18 @@ import { Text, Image, View, ScrollView } from "react-native";
 import Navbar from "../components/Navbar/Navbar"
 import Hero from "../components/Hero/Hero";
 import Arrival from "../components/Arrival/Arrival";
+import { ProductContextProvider } from "../contexts/productContext"
+import Collection from "../components/Collection/Collection";
 
 const Home = () => {
     return (
         <ScrollView style={{ flex: 1 }}>
         <Navbar />
         <Hero />
-        <Arrival />
+        <ProductContextProvider>
+            <Arrival />
+            <Collection />
+            </ProductContextProvider>
         </ScrollView>
     )
 }
