1435c1f205c3904356c0eddb14ab96ae8e1159a4
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-10-15 15:41:25 +0100

    Create blog.js

---
+++ app/Blog/blog.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
+import * as Font from 'expo-font';
+
+const BlogScreen = () => {
+    return (
+        <View>
+      <Text>This is the Blog screen</Text>
+    </View>
+    )
+}
+
+export default BlogScreen
