66770e0bb381c7f5589fd4dd55789aa794b6d731
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-10-21 14:33:40 +0100

    Update productContext.js

--- contexts/productContext.js
+++ contexts/productContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState } from 'react';
-import { arrivalData, foryouData, BlogData } from './data';
+import { arrivalData, foryouData, BlogData, ProductData } from './data';
 
 export const ProductContext = createContext();
 
@@ -7,9 +7,10 @@ export const ProductContextProvider = ({ children }) => {
   const [newArrival, setNewArrival] = useState(arrivalData);
   const [forYou, setForYou] = useState(foryouData);
   const [blogs, setBlogs] = useState(BlogData);
+  const [products, setProducts] = useState(ProductData);
 
   return (
-    <ProductContext.Provider value={{ newArrival, setNewArrival, forYou, setForYou, blogs, setBlogs }}>
+    <ProductContext.Provider value={{ newArrival, setNewArrival, forYou, setForYou, blogs, setBlogs, products, setProducts }}>
       {children}
     </ProductContext.Provider>
   );
