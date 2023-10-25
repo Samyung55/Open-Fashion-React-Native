a21b2fc72799fe25a3545f4673d3d7c6659bef64
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-10-25 22:19:07 +0100

    Update cartContext.js

--- contexts/cartContext.js
+++ contexts/cartContext.js
@@ -1,4 +1,5 @@
-import { createContext, useReducer, useContext } from 'react'
+import { createContext, useReducer, useContext, useEffect } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import cartReducer from './reducers/cartReducer'
 
@@ -10,6 +11,25 @@ let cartState = {
 
 export const CartProvider = ({ children }) => {
     const [cart, dispatch] = useReducer(cartReducer, cartState)
+
+    useEffect(() => {
+        // Load cart items from AsyncStorage when the app starts
+        (async () => {
+          try {
+            const cartItems = await AsyncStorage.getItem('cartItems');
+            if (cartItems) {
+              dispatch({ type: 'INITIALIZE_CART', payload: JSON.parse(cartItems) });
+            }
+          } catch (error) {
+            console.error('Error loading cart items:', error);
+          }
+        })();
+      }, []);
+    
+      useEffect(() => {
+        // Save cart items to AsyncStorage whenever the cart state changes
+        AsyncStorage.setItem('cartItems', JSON.stringify(cart.cartItems));
+      }, [cart.cartItems]);
 
     const value = {
         cart, dispatch
