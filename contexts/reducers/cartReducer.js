74edc753a4434368d902ea111ae7a2832c37a037
Sam Fred <123533544+Samyung55@users.noreply.github.com>
2023-10-25 12:25:14 +0100

    Update cartReducer.js

--- contexts/reducers/cartReducer.js
+++ contexts/reducers/cartReducer.js
@@ -1,39 +1,52 @@
-import * as actionType from '../utils/cart';
 
-const initialState = {
-  cartItems: [], // Initial state for cartItems
-};
+const cartActionTypes = {
+    ADD_TO_CART: 'ADD_TO_CART',
+    REMOVE_FROM_CART: 'REMOVE_FROM_CART',
+    INCREASE_QUANTITY: 'INCREASE_QUANTITY',
+    DECREASE_QUANTITY: 'DECREASE_QUANTITY',
+  };
+  
 
-const cartReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case actionType.ADD_TO_CART:
-      const item = action.payload;
-
-      const existItem = state.cartItems.find((i) => i.id === item.id);
-      if (existItem) {
-        return {
-          ...state,
-          cartItems: state.cartItems.map((i) =>
-            i.id === item.id ? { ...i, quantity: i.quantity + 1 } : i
-          ),
-        };
-      } else {
-        // If it doesn't exist, add the item to the cartItems array
-        return {
-          ...state,
-          cartItems: [...state.cartItems, { ...item, quantity: 1 }],
-        };
-      }
-
-    case actionType.REMOVE_FROM_CART:
-      return {
-        ...state,
-        cartItems: state.cartItems.filter((i) => i.id !== action.payload),
-      };
-
-    default:
-      return state;
-  }
-};
-
-export default cartReducer;
+export const cartReducer = (state, action) => {
+    switch (action.type) {
+      case cartActionTypes.ADD_TO_CART:
+        const newItem = action.payload;
+        const existingItemIndex = state.cartItems.findIndex(item => item.id === newItem.id);
+  
+        if (existingItemIndex !== -1) {
+          // If the item already exists in the cart, update its quantity
+          const updatedCartItems = [...state.cartItems];
+          updatedCartItems[existingItemIndex].quantity += newItem.quantity;
+          return { ...state, cartItems: updatedCartItems };
+        } else {
+          // If the item is not in the cart, add it
+          return { ...state, cartItems: [...state.cartItems, newItem] };
+        }
+  
+      case cartActionTypes.REMOVE_FROM_CART:
+        const itemIdToRemove = action.payload;
+        const updatedCartItems = state.cartItems.filter(item => item.id !== itemIdToRemove);
+        return { ...state, cartItems: updatedCartItems };
+  
+      case cartActionTypes.INCREASE_QUANTITY:
+        const itemIdToIncrease = action.payload;
+        const increasedCartItems = state.cartItems.map(item =>
+          item.id === itemIdToIncrease ? { ...item, quantity: item.quantity + 1 } : item
+        );
+        return { ...state, cartItems: increasedCartItems };
+  
+      case cartActionTypes.DECREASE_QUANTITY:
+        const itemIdToDecrease = action.payload;
+        const decreasedCartItems = state.cartItems.map(item =>
+          item.id === itemIdToDecrease && item.quantity > 1
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+        );
+        return { ...state, cartItems: decreasedCartItems };
+  
+      default:
+        return state;
+    }
+  };
+  
+  export default cartReducer
